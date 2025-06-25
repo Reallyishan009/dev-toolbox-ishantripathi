cd798bafb59d39bcb047fe9455ab57cd61d18076
Reallyishan009 <ishantripathidoc@gmail.com>
2025-06-26 03:10:02 +0530

    retry 5

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const cors = require("cors");
-app.use(cors());
-
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 app.use(bodyParser.json());
 
